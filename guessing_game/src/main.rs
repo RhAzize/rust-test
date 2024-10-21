e096d1fcc46e70fe3d23138e8dd84ec19f14ffa8
ARH <131004018+RhAzize@users.noreply.github.com>
2024-10-22 00:45:56 +0200

    input test

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,16 @@
+use std::io::{self, Write};
+use std::time::Instant;
+
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("Guess the number!\n Please input your guess.");
+    io::stdout().flush().unwrap();
+
+    let mut guess = String::with_capacity(10);
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess.trim());
 }
