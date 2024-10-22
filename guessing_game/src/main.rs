0b844b15f56b0d6432b838aca2712e46e3a901c9
ARH <131004018+RhAzize@users.noreply.github.com>
2024-10-23 01:09:20 +0200

    guessing game completed

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,38 @@
+use rand;
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io::{self, Write};
 
 fn main() {
+    //Gen un num de 0 a 100
+    let secret: u32 = rand::thread_rng().gen_range(0..=100);
 
-    println!("Guess the number!\n Please input your guess.");
-    io::stdout().flush().unwrap();
+    loop {
+        println!("Guess the number!\nPlease input your guess.");
+        io::stdout().flush().unwrap();
 
-    let mut guess = String::with_capacity(10);
+        // On créer une var mutable ac une capacité de 10
+        let mut guess = String::with_capacity(10);
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess.trim());
+        // On convertit la string en u32
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        // Puis on compare le guess avecc le secret
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too small! 🤬"),
+            Ordering::Greater => println!("Too big! 😂"),
+            Ordering::Equal => {
+                println!("You WIN!!!🦀");
+                break;
+            }
+        }
+        println!("You guessed: {}", guess);
+    }
 }
