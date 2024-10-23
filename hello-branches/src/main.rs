33297c408f01d17c55086d1a088c1ac2fc2fea84
ARH <131004018+RhAzize@users.noreply.github.com>
2024-10-24 00:34:34 +0200

    loop,fibonacci,celsuis test

--- hello-branches/src/main.rs
+++ hello-branches/src/main.rs
@@ -1,26 +1,95 @@
 fn main() {
-    //Condition 
+    //Condition
     condition();
 
     //loop
     looping();
+
+    //while
+    while_loop();
+
+    //for
+    for_loop();
+
+    //test conversion fahrebheit vers celsius
+    let celsius = fahrenheit_to_celsius(95);
+    println!("95 °F = {celsius} °C\n", celsius = celsius);
+
+    //fibonacci
+    let n:i32 = 10;
+    let response:i32 = fibonacci(n);
+    println!("fibonacci({n}) = {response}\n", n = n, response = response);
 }
 
+////Pseudo-code
+// function fibonacci(n) {
+//  if (n == 0) return 0
+//  if (n == 1) return 1
+//  prev = 0
+//  curr = 1
+//  for i from 2 to n {
+//   next = prev + curr
+//   prev = curr
+//   curr = next
+//  }
+//  return curr
+// }
+
+fn fibonacci(n: i32) -> i32 {
+    println!("-- Fibonacci --");
+    if n == 0 {
+        0
+    } else if n == 1 {
+        1
+    } else {
+        let mut prev = 0;
+        let mut curr = 1;
+        for index in 2..=n {
+            let next = prev + curr;
+            prev = curr;
+            curr = next;
+            println!("F({index}) = {next}");
+        }
+        curr
+    }
+}
+fn fahrenheit_to_celsius(value: i32) -> i32 {
+    println!("-- Fahrenheit to Celsius --");
+
+    //Formule : (86 °F − 32) × 5/9 = 30 °C
+    (value - 32) * 5 / 9
+}
 fn condition() {
+    println!("-- Condition --");
+    let x: bool = true;
 
-    let x:bool = true;
+    let compare = if x { true } else { false };
 
-    let compare = if x { 1 } else {0}; 
-
-    println!("Compare {}", compare);
-
+    println!("Result = {}\n", compare);
 }
 
+fn for_loop() {
+    println!("-- For --");
+    for number in (1..10).rev() {
+        println!("number {number}");
+    }
+    println!("\n");
+}
+fn while_loop() {
+    println!("-- While --");
+    let mut counter = 0;
+
+    while counter < 10 {
+        println!("counter {counter}");
+        counter += 1;
+    }
+    println!("\n");
+}
 fn looping() {
+    println!("-- Loop --");
     let mut counter = 0;
     let result = loop {
-
-        counter +=1;
+        counter += 1;
 
         println!("counter {counter}");
 
@@ -29,7 +98,6 @@ fn looping() {
             break counter * 2;
         }
     };
-    
-    println!("result = {result}"); // 20
 
+    println!("result = {result}\n"); // 20
 }
