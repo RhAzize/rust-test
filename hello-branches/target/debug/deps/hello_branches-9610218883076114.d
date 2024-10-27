3d0f71145b3e829e4012633cf6de2e1b74b9c704
ARH <131004018+RhAzize@users.noreply.github.com>
2024-10-27 02:24:20 +0200

    Suppression du dossier target de l'index et application de .gitignore

--- hello-branches/target/debug/deps/hello_branches-9610218883076114.d
+++
@@ -1,5 +0,0 @@
-/Users/azize/Documents/Projects/rust-test/hello-branches/target/debug/deps/hello_branches-9610218883076114: src/main.rs
-
-/Users/azize/Documents/Projects/rust-test/hello-branches/target/debug/deps/hello_branches-9610218883076114.d: src/main.rs
-
-src/main.rs:
--- hello-branches/target/debug/hello-branches.d
+++
@@ -1,1 +0,0 @@
-/Users/azize/Documents/Projects/rust-test/hello-branches/target/debug/hello-branches: /Users/azize/Documents/Projects/rust-test/hello-branches/src/main.rs
